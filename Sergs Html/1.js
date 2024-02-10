266970e0cd9f65696333529f5f464466101a5033
Emiyashiro20 <159637175+Emiyashiro20@users.noreply.github.com>
2024-02-10 23:55:17 +0800

    Add files via upload

---
+++ Sergs Html/1.js
@@ -0,0 +1,24 @@
+document.getElementById('next').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').appendChild(lists[0]);
+}
+document.getElementById('prev').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').prepend(lists[lists.length - 1]);
+}
+function calculateStrokeTextCSS(steps) {
+  var css = "";
+  for (var i = 0; i < steps; i++) {
+    var angle = (i * 2 * Math.PI) / steps;
+    var cos = Math.round(10000 * Math.cos(angle)) / 10000;
+    var sin = Math.round(10000 * Math.sin(angle)) / 10000;
+    css +=
+      "calc(var(--stroke-width) * " +
+      cos +
+      ") calc(var(--stroke-width) * " +
+      sin +
+      ") 0 var(--stroke-color),";
+  }
+
+  return css;
+}
